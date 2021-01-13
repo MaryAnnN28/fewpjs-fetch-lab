614210050b9f26e29244430b2bac67d1d6c8995f
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-01-13 09:58:40 -0500

    done

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books') .then(function(response) {
+    return response.json();
+  }) .then(function(json) {
+    console.log(json)
+    renderBooks(json)
+  });
 }
 
 function renderBooks(books) {
@@ -14,3 +19,5 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
 })
+
+
